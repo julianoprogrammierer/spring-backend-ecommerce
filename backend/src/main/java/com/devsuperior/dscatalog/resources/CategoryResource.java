a810930bf400a62e6dfbf14b359dad696f0a4443
Juliano Roberto Machado <julianoprogrammierer@gmail.com>
2023-03-29 19:34:59 -0300

    feat:CategoryResources class created.

---
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -0,0 +1,31 @@
+package com.devsuperior.dscatalog.resources;
+
+import com.devsuperior.dscatalog.entities.Category;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Class CategoryResources.
+ */
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+  /**
+   * Class CategoryResources.
+   */
+  @GetMapping
+  public ResponseEntity<List<Category>> findAll() {
+
+    List<Category> list = new ArrayList<>();
+    list.add(new Category(1L, "Books"));
+    list.add(new Category(2L, "Eletronics"));
+    return ResponseEntity.ok().body(list);
+  }
+
+}
