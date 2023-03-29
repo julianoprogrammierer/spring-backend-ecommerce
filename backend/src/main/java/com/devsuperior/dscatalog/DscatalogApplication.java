29bdc3df5b74b00a2a6045b24f890a7b3fd3255a
Juliano Roberto Machado <julianoprogrammierer@gmail.com>
2023-03-28 22:39:43 -0300

    backend-spring-create

---
+++ backend/src/main/java/com/devsuperior/dscatalog/DscatalogApplication.java
@@ -0,0 +1,16 @@
+package com.devsuperior.dscatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main method.
+ */
+@SpringBootApplication
+public class DscatalogApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DscatalogApplication.class, args);
+  }
+
+}
