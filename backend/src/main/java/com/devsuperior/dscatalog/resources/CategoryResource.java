cf85e3f4ddc892c11a0332ead5c80ab16d0f5233
Juliano Roberto Machado <julianoprogrammierer@gmail.com>
2023-03-30 07:25:51 -0300

    feat: create classes category.

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -1,12 +1,13 @@
 package com.devsuperior.dscatalog.resources;
 
 import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 
@@ -16,15 +17,13 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
-  /**
-   * Class CategoryResources.
-   */
+  @Autowired
+  private CategoryService service;
+
   @GetMapping
   public ResponseEntity<List<Category>> findAll() {
+    List<Category> list = service.findAll();
 
-    List<Category> list = new ArrayList<>();
-    list.add(new Category(1L, "Books"));
-    list.add(new Category(2L, "Eletronics"));
     return ResponseEntity.ok().body(list);
   }
 
