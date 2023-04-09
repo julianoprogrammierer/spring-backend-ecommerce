9d2a1b6d6abfed268797ed7901789d66aa9b529f
Juliano Roberto Machado <julianoprogrammierer@gmail.com>
2023-04-09 18:06:03 -0300

    (1-17~18)feat: Post Category.

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -6,9 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 
@@ -21,6 +25,11 @@ public class CategoryResource {
   @Autowired
   private CategoryService service;
 
+  /**
+   * Find all response entity.
+   *
+   * @return the response entity
+   */
   @GetMapping
   public ResponseEntity<List<CategoryDTO>> findAll() {
     List<CategoryDTO> list = service.findAll();
@@ -28,11 +37,31 @@ public class CategoryResource {
     return ResponseEntity.ok().body(list);
   }
 
+  /**
+   * Find by id response entity.
+   *
+   * @param id the id
+   * @return the response entity
+   */
   @GetMapping(value = "/{id}")
   public ResponseEntity<CategoryDTO> findById(@PathVariable Long id) {
     CategoryDTO dto = service.findById(id);
 
     return ResponseEntity.ok().body(dto);
+  }
+
+  /**
+   * Insert response entity.
+   *
+   * @param dto the dto
+   * @return the response entity
+   */
+  @PostMapping
+  public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto) {
+    dto = service.insert(dto);
+    URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+            .buildAndExpand(dto.getId()).toUri();
+    return ResponseEntity.created(uri).body(dto);
   }
 
 }
--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,22 +1,31 @@
 package com.devsuperior.dscatalog.services;
 
+
 import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
 import com.devsuperior.dscatalog.services.exceptions.EntityNotFoundException;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
+/**
+ * The type Category service.
+ */
 @Service
 public class CategoryService {
   @Autowired
   private CategoryRepository repository;
 
+  /**
+   * Find all list.
+   *
+   * @return the list
+   */
   @Transactional(readOnly = true)
   public List<CategoryDTO> findAll() {
     List<Category> list = repository.findAll();
@@ -27,10 +36,30 @@ public class CategoryService {
     return list.stream().map(CategoryDTO::new).collect(Collectors.toList());
   }
 
+  /**
+   * Find by id category dto.
+   *
+   * @param id the id
+   * @return the category dto
+   */
   @Transactional(readOnly = true)
   public CategoryDTO findById(Long id) {
     Optional<Category> obj = repository.findById(id);
     Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not Found"));
+    return new CategoryDTO(entity);
+  }
+
+  /**
+   * Insert category dto.
+   *
+   * @param dto the dto
+   * @return the category dto
+   */
+  @Transactional
+  public CategoryDTO insert(CategoryDTO dto) {
+    Category entity = new Category();
+    entity.setName(dto.getName());
+    entity = repository.save(entity);
     return new CategoryDTO(entity);
   }
 }
