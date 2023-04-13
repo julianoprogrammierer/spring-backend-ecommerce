a54575ac512b1443de28793e8461f28d97cf0277
Juliano Roberto Machado <julianoprogrammierer@gmail.com>
2023-04-13 00:36:55 -0300

    (1-25~26)feat: create product mvc GET.

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Product.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Product.java
@@ -3,7 +3,6 @@ package com.devsuperior.dscatalog.entities;
 import java.io.Serializable;
 import java.time.Instant;
 import java.util.HashSet;
-import java.util.Objects;
 import java.util.Set;
 import javax.persistence.*;
 
@@ -11,34 +10,36 @@ import javax.persistence.*;
 @Table(name = "tb_product")
 public class Product implements Serializable {
   private static final long serialVersionUID = 1L;
+
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String name;
+
   @Column(columnDefinition = "TEXT")
   private String description;
   private Double price;
   private String imgUrl;
+
   @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
   private Instant date;
+
   @ManyToMany
-  @JoinTable(
-          name = "tb_product_category",
+  @JoinTable(name = "tb_product_category",
           joinColumns = @JoinColumn(name = "product_id"),
-          inverseJoinColumns = @JoinColumn(name = "category_id")
-  )
+          inverseJoinColumns = @JoinColumn(name = "category_id"))
   Set<Category> categories = new HashSet<>();
 
   public Product() {
   }
 
-  public Product(Long id, String name, String description, Double price, String imgUrl, Instant data) {
+  public Product(Long id, String name, String description, Double price, String imgUrl, Instant date) {
     this.id = id;
     this.name = name;
     this.description = description;
     this.price = price;
     this.imgUrl = imgUrl;
-    this.date = data;
+    this.date = date;
   }
 
   public Long getId() {
@@ -94,15 +95,27 @@ public class Product implements Serializable {
   }
 
   @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (!(o instanceof Product)) return false;
-    Product product = (Product) o;
-    return Objects.equals(getId(), product.getId());
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((id == null) ? 0 : id.hashCode());
+    return result;
   }
 
   @Override
-  public int hashCode() {
-    return Objects.hash(getId());
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Product other = (Product) obj;
+    if (id == null) {
+      if (other.id != null)
+        return false;
+    } else if (!id.equals(other.id))
+      return false;
+    return true;
   }
 }
